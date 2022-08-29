f9457706c94e922cc952409b75b0a7e71948fea2
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-08-29 16:13:30 -0300

    feat: fully working tic-tac-toe

--- src/components/Matrix/index.tsx
+++ src/components/Matrix/index.tsx
@@ -6,6 +6,8 @@ import { useState,useEffect } from 'react'
 export const Matrix = () =>{
     const [matrix,setMatrix] = useState([['','',''],['','',''],['','','']])
     const [turn,setTurn] = useState('x')
+    const [winner,setWinner] = useState('false')
+    const [tie,setTie] = useState('false')
 
     const handleClick = (row: number,column: number) =>{
         var newMatrix = matrix
@@ -17,11 +19,59 @@ export const Matrix = () =>{
             setTurn('x')
         }
         setMatrix(newMatrix)
+        setWinner(whoWon())
+        if (winner == 'false'){
+            setTie(isTie())
+        }
+    }
+
+    const whoWon = () =>{
+        for (let i=0;i<3;i++){
+            if(matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2] && matrix[i][0] != ''){
+                return matrix[i][0]
+            }
+            if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i] && matrix[0][i] != ''){
+                return matrix[0][i]
+            }
+
+        }
+        if (matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2] && matrix[0][0] != '' ){
+            return matrix[0][0]
+        }
+        if (matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0] && matrix[0][2] != ''){
+            return matrix[0][2]
+        }
+        return 'false'
+    }
+
+    const isTie = () =>{
+        for (let i=0;i<3;i++){
+            for (let j=0;j<3;j++){
+                if (matrix[i][j] == ''){
+                    return 'false'
+                }
+            }
+        }
+        return 'true'
     }
 
     useEffect(()=>{
-        console.log("useeffect")
-    },[matrix])
+        if (winner != 'false'){
+            alert(`${winner} has won!`)
+            setWinner('false')
+            setTie('false')
+            setMatrix([['','',''],['','',''],['','','']])
+        }
+        else if (tie != 'false'){
+            alert('Tie!')
+            setWinner('false')
+            setTie('false')
+            setMatrix([['','',''],['','',''],['','','']])
+        }
+        
+        
+        
+    },[matrix,winner,tie])
 
     return(
         <Table bordered>
